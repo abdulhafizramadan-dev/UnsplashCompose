1f2e497024906ca6a5e71498a8fd0c29d96281f1
abdulhafizramadan <abdulhafizramadan.dev@gmail.com>
2022-09-28 17:11:11 +0700

    | Setup Room Entity

---
+++ app/src/main/java/com/ahr/unsplashcompose/data/local/entity/UnsplashImageEntity.kt
@@ -0,0 +1,41 @@
+package com.ahr.unsplashcompose.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ahr.unsplashcompose.util.Constant.UNSPLASH_IMAGE_TABLE
+
+@Entity(tableName = UNSPLASH_IMAGE_TABLE)
+data class UnsplashImageResponse(
+
+	@PrimaryKey(autoGenerate = false)
+	val id: String,
+
+	val likes: Int,
+
+	@Embedded
+	val user: User,
+
+	@Embedded
+	val urls: Urls
+)
+
+data class User(
+
+	@Embedded
+	@ColumnInfo(name = "links")
+	val userLinks: UserLinks,
+
+	val username: String
+)
+
+data class UserLinks(
+
+	val html: String
+)
+
+data class Urls(
+
+	val regular: String
+)
---
+++ app/src/main/java/com/ahr/unsplashcompose/util/Constant.kt
@@ -0,0 +1,7 @@
+package com.ahr.unsplashcompose.util
+
+object Constant {
+
+    const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
+
+}
